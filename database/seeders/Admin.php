12c1e0d4b18d89cf8eb0e89aa4f9d4f7862655e0
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-05-19 14:39:29 +0300

    migration, model ve seeder oluşturuldu

---
+++ database/seeders/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Str;
+
+class Admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "name" => "Admin",
+            "surname" => "admin",
+            "email" => "admin@gmail.com",
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            "birth_date" => Date("Y-m-d"),
+            "type" => 1,
+        ]);
+    }
+}
